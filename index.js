3ec21f40209849d9262b1b4d9e6022f1937da863
root <root@ip-172-31-9-163.ap-south-1.compute.internal>
2026-03-17 19:26:38 +0000

    7

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('DevOps Project Running on EKS 🚀ohooooo');
+  res.send('DevOps Project Running on EKS 🚀ohoooowwwwwwwo');
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
